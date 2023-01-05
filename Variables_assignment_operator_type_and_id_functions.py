b9e878df40632fd901974886ed9b91c356387192
Airon <maksim285@mail.ru>
2023-01-05 17:17:15 +0700

    First lesson

---
+++ Variables_assignment_operator_type_and_id_functions.py
@@ -0,0 +1,21 @@
+# a = 7
+# # a = 6.8
+# b = 'Hello'
+# b = 9
+# b = -9.1
+# # b = a
+# print(a, b)
+# var_A = 'Hello'
+# print(var_A)
+
+s = 'Hello'
+z, q = 45, 6.7
+print(z, q)
+print(id(z), id(q))
+z, q = q, z
+print(z, q)
+print(id(z), id(q))
+print(type(z), type(q), type(s))
+# a = b = c = 0
+# print(a, b, c)
+# print(id(a), id(b), id(c))
