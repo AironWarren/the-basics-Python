9a8b10197dfa7a69cf1252cdcf6ee1a705899850
Airon <maksim285@mail.ru>
2023-01-06 20:06:29 +0700

    third lesson

--- Numbers_and_operations_on_them.py
+++ Numbers_and_operations_on_them.py
@@ -10,5 +10,11 @@ print(f"x = {b}; y = {c}")
 a, b = map(float, input("Введите стороны прямоугольника: ").split())
 
 print(type(a))
-print(f"Периметр: = {2 * (a + b)}")
+prints1, s2 = input().split()
+
+s1 = s1 + ' '
+s2 = s2 + ' '
+s3 = (s1 * 2) + (s2 * 3)
+
+print(s3.rstrip(' '))(f"Периметр: = {2 * (a + b)}")
 
---
+++ line.py
@@ -0,0 +1,35 @@
+s1 = "Максим"
+s2 = 'Maxim'
+
+text = '''А
+я
+хорош'''
+
+m = s1 + s2
+
+d = 'ha' * 5
+
+print(s1)
+print(s2)
+print(m)
+print(d)
+print(len(d))
+
+s = 'Hello python'
+print(s[len(s)-1])
+
+print(s[2:7])
+
+a = s[:]
+
+print(id(a))
+print(id(s))
+
+
+# s1, s2 = input().split()
+#
+# s1 = s1 + ' '
+# s2 = s2 + ' '
+# s3 = (s1 * 2) + (s2 * 3)
+#
+# print(s3.rstrip(' '))
